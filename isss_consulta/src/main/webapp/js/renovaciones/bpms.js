47d7e1317c757bae97885889be9a70f2740c516d
Jose Ramirez <r2jf@hotmail.com>
2016-08-27 18:41:39 -0600

    Tareas + iniciaProceso

--- isss_consulta/src/main/webapp/js/renovaciones/bpms.js
+++ isss_consulta/src/main/webapp/js/renovaciones/bpms.js
@@ -56,36 +56,22 @@ var iniciaProceso = function (processDefinitionID, processDeployment) {
         },
         url: server + '/business-central/rest/runtime/' + processDeployment + '/process/' + processDefinitionID + '/start',
         success: function (data) {
-            //console.log(data);
             console.log('Proceso iniciado con exito, ID=' + data.id);
-            //processID = data.id;
-            //guardo las variables processID y clientID en localStorage (validar que no se pueda usar este boton (btnTerminar) si no ha cargado un clientID)
-            //var bpms = {
-            //    codCliente: $('#clientes_clienteId').val(),
-            //    processID: data.id
-            //};
-            ////lo convierto a JSON
-            //bpms = JSON.stringify(bpms);
-            ////por si lo quiero encriptar
-            ////bpms = btoa(bpms);
-            ////lo guardo en el localStorage
-            //localStorage.setItem('bpmsls', bpms);
-            //console.log('funcion guardar en localStorage completada');
-            //console.log(bpms);
             console.log('Obteniendo tarea recien creada...');
             obtieneTareaPorProceso(data.id, 1);
         },
         error: function () {
+            console.log('Error ejecutando metodo iniciaProceso');
             //para pruebas offline
-            console.log('pruebas offline: Inicia Proceso =====')
-            var json = {
-                "process-id": "ejemploSubida.demoAPI2",
-                "id": "26244",
-                "state": "1",
-                "parentProcessInstanceId": "0",
-                "status": "SUCCESS",
-                "url": "/business-central/rest/runtime/bpmDemostracion:ejemploSubida:1.0/process/ejemploSubida.demoAPI2/start"
-            };
+            //console.log('pruebas offline: Inicia Proceso =====')
+            //var json = {
+            //    "process-id": "ejemploSubida.demoAPI2",
+            //    "id": "26244",
+            //    "state": "1",
+            //    "parentProcessInstanceId": "0",
+            //    "status": "SUCCESS",
+            //    "url": "/business-central/rest/runtime/bpmDemostracion:ejemploSubida:1.0/process/ejemploSubida.demoAPI2/start"
+            //};
             //console.log(json);
             //console.log(json.id);
             //var bpms = {
@@ -100,10 +86,6 @@ var iniciaProceso = function (processDefinitionID, processDeployment) {
             //localStorage.setItem('bpmsls', bpms);
             //console.log('funcion guardar en localStorage completada');
             //console.log(bpms);
-
-            console.log('Obteniendo tarea recien creada...');
-            obtieneTareaPorProceso(json.id, 1);
-
         }
     })
 }
@@ -137,46 +119,7 @@ var obtieneTareaPorProceso = function(processID, bandCompletar) {
             });
         },
         error: function () {
-            //para pruebas offline de formateo
-            console.log('pruebas Offline Obtiene Tarea =====');
-            var json = {
-                "status": null,
-                "url": null,
-                "index": null,
-                "commandName": null,
-                "taskSummaryList": [
-                   {
-                       "@class": "org.kie.services.client.serialization.jaxb.impl.task.JaxbTaskSummary",
-                       "id": 801,
-                       "name": "Tarea 1",
-                       "subject": "",
-                       "description": "",
-                       "status": "Reserved",
-                       "priority": 0,
-                       "skipable": true,
-                       "actualOwnerId": "bpmadmin",
-                       "createdById": "bpmadmin",
-                       "createdOn": 1463584786190,
-                       "activationTime": 1463584786190,
-                       "expirationTime": null,
-                       "processInstanceId": 26243,
-                       "processId": "ejemploSubida.demoAPI2",
-                       "processSessionId": 546,
-                       "deploymentId": "bpmDemostracion:ejemploSubida:1.0",
-                       "quickTaskSummary": false,
-                       "parentId": -1,
-                       "potentialOwners": null
-                   }
-                ],
-                "pageNumber": null,
-                "pageSize": null
-            };
-            $.each(json.taskSummaryList, function (index, element) {
-                if (element.status == 'Reserved') {
-                    console.log('Iniciando tarea encontrada con ID=' + element.id);
-                    iniciaTarea(element.id, processID, bandCompletar);
-                }
-            });
+            console.log('Error ejecutando metodo obtieneTareaPorProceso');
         }
     })
 }
@@ -204,32 +147,15 @@ var iniciaTarea = function(tareaID, processID, bandCompletar) {//if bandCompleta
             if (bandCompletar == 1) {
                 console.log('completando tarea recien iniciada...');
                 var parametros = {
-                    "map_cod_cliente": '"' + $('#clientes_clienteId').val() + '"',
-                    "map_nit_cliente": '"' + $('#clientes_nithidden').val() + '"',
-                    "map_nombreCliente": '"' + $('#clientes_nombrehidden').val() + '"'
+                    "map_numeroAfiliacion": '"' + $('#clientes_numeroAfiliacion').val() + '"',
+                    "map_DUI": '"' + $('#clientes_DUI').val() + '"',
+                    "map_nombreAfiliado": '"' + $('#clientes_nombreAfiliado').val() + '"'
                 };
                 completaTarea(tareaID, processID, parametros, bandCompletar);
             }
         },
         error: function () {
-            //para pruebas offline de formateo
-            console.log('pruebas Offline Inicia Tarea =====');
-            var json = {
-                "status": "SUCCESS",
-                "url": "/business-central/rest/task/841/start"
-            };
-            console.log(json);
-            if (json.status == 'SUCCESS') {
-                console.log('Tarea con ID ' + tareaID + ' iniciada');
-                taskID = tareaID;
-            }
-            if (bandCompletar == 1) {
-                console.log('completando tarea recien iniciada...');
-                parametros = {
-                    "map_cod_cliente": '"' + $('#clientes_clienteId').val() + '"'
-                };
-                completaTarea(tareaID, processID, parametros, bandCompletar);
-            }
+            console.log('Error ejecutando metodo iniciaTarea');
         }
     })
 }
@@ -260,21 +186,7 @@ var completaTarea = function(taskID, processID, dataMappings, bandCompletar) {
             //$.siguientePaso(processID, $("#paso_siguiente").val(), $('#clientes_clienteId').val());
         },
         error: function () {
-            //para pruebas offline de formateo
-            console.log('pruebas Offline Completar Tarea =====');
-            var json = {
-                "status": "SUCCESS",
-                "url": "/business-central/rest/task/842/complete",
-                "message": null
-            };
-            //console.log(json);
-            if (json.status == 'SUCCESS') {
-                console.log('Tarea con ID ' + taskID + ' completada');
-            }
-            //$.siguientePaso(processID, $("#paso_siguiente").val(), $('#clientes_clienteId').val());
-            if (bandCompletar == 1) {
-                botonSiguiente(processID);
-            }
+            console.log('Error ejecutando metodo completaTarea');
         }
     })
 }
@@ -439,9 +351,9 @@ function obtieneTareas(potentialOwner, estado) {
 
 //taskElement es el json completo de la tarea, procVariables son todas las variables del proceso al cual pertenece esa tarea
 function construyeLinea(taskElement, procVariables) {
-    var cod_cliente = procVariables.variables.cod_cliente;
-    var nit_cliente = procVariables.variables.nit_cliente;
-    var nombre_cliente = procVariables.variables.nombreCliente;
+    var cod_afiliacion = procVariables.variables.numeroAfiliacion;
+    var dui_cliente = procVariables.variables.DUI;
+    var nombre_afiliado = procVariables.variables.nombreAfiliado;
     var procID = taskElement.processInstanceId;
     var URLtaskForm = taskElement.description;
     var estado = taskElement.status;
@@ -453,14 +365,14 @@ function construyeLinea(taskElement, procVariables) {
     $('#tablaTareas').append($('<tr>').append(
                 '<td>' + taskElement.id + '</td>' +
                 '<td>' + taskElement.name + '</td>' +
-                '<td>' + nombre_cliente + ': ' + cod_cliente + '</td>' +
-                '<td>' + nit_cliente + '</td>' +
+                '<td>' + nombre_afiliado + ': ' + cod_afiliacion + '</td>' +
+                '<td>' + dui_cliente + '</td>' +
                 '<td>' + taskElement.priority + '</td>' +
                 '<td>' + (taskElement.status == 'Reserved' || 'Ready' ? 'Pendiente' : 'En Proceso') + '</td>' +
                 '<td>' + formatoFechaISO(taskElement.createdOn) + '</td>'
                     ).attr({
                         'class': 'clickableRow',
-                        'onclick': 'window.location="' + URLtaskForm + '?clientesID=' + cod_cliente + '&processid=' + procID + '&taskID=' + taskElement.id + '&estado=' + estado + '"'
+                        'onclick': 'window.location="' + URLtaskForm + '?clientesID=' + cod_afiliacion + '&processid=' + procID + '&taskID=' + taskElement.id + '&estado=' + estado + '"'
                     })
                 );
 }
@@ -482,29 +394,7 @@ function obtieneVariablesProceso(taskElement) {
             //$('#resultado').html(JSON.stringify(data));
         },
         error: function () {
-            //para pruebas offline de formateo
-            console.log('pruebas offline obtiene Variables ==');
-            var json = {
-                "status": "SUCCESS",
-                "url": "/business-central/rest/runtime/bpmDemostracion:ejemploSubida:1.0/withvars/process/instance/26308",
-                "variables": [
-                   {
-                       "key": "cod_cliente",
-                       "value": "123456"
-                   },
-                   {
-                       "key": "initiator",
-                       "value": "bpmadmin"
-                   }
-                ],
-                "processInstance": {
-                    "process-id": "ejemploSubida.demoAPI2",
-                    "id": "26308",
-                    "state": "1",
-                    "parentProcessInstanceId": "0"
-                }
-            };
-            construyeLinea(taskElement, json);
+            console.log('Error ejecutando metodo obtieneVariablesProceso');
         }
     })
 }
@@ -683,36 +573,6 @@ function obtieneVariablesIngresoEgreso() {//le mando el deployment id que es x:x
     })
 }
 
-function obtieneVariableSolicitud() {//le mando el deployment id que es x:x:x y el processid (esto ya esta en la URL y lo seteo en cargapaginainiciatarea)
-    $.ajax({
-        type: 'GET',
-        xhrFields: {
-            withCredentials: true,
-        },
-        cache: true,
-        dataType: 'json',
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader('Authorization', 'Basic ' + btoa(window.usuario + ':' + window.passwd));
-        },
-        url: server + '/business-central/rest/runtime/' + window.processDeployment + '/withvars/process/instance/' + processid,
-        success: function (data) {
-            console.log(data);
-            console.log('ID de Solicitud= ' + data.variables.Idsolicitud);
-            //llamo el WS 
-            $.getJSON("http://172.26.20.122:8080/TestRestAPP_Digicel/webresources/ActivacionRenovacionSPP/getActivacionSPP",
-            { Idsolicitud: data.variables.Idsolicitud },
-            mostrarDatos);
-
-        },
-        error: function () {
-            //para pruebas offline de formateo
-            console.log('Error llamando a metodo');
-            $.getJSON("http://172.26.20.122:8080/TestRestAPP_Digicel/webresources/ActivacionRenovacionSPP/getActivacionSPP",
-            { Idsolicitud: '123456' },
-            mostrarDatos);
-        }
-    })
-}
 
 function obtieneTareasIndex() {
     $.ajax({
@@ -753,119 +613,10 @@ function obtieneTareasIndex() {
         },
         error: function () {
             //para pruebas offline de formateo
-            console.log('pruebas offline Obtiene tareas INDEX ===');
-            var json = {
-                "status": null,
-                "url": null,
-                "index": null,
-                "commandName": null,
-                "taskSummaryList": [
-                   {
-                       "@class": "org.kie.services.client.serialization.jaxb.impl.task.JaxbTaskSummary",
-                       "id": 801,
-                       "name": "Tarea 1",
-                       "subject": "",
-                       "description": "../renovaciones/02-detalle-cuenta.html",
-                       "status": "InProgress",
-                       "priority": 0,
-                       "skipable": true,
-                       "actualOwnerId": "bpmadmin",
-                       "createdById": "bpmadmin",
-                       "createdOn": 1463584786190,
-                       "activationTime": 1463584786190,
-                       "expirationTime": null,
-                       "processInstanceId": 26209,
-                       "processId": "ejemploSubida.demoAPI2",
-                       "processSessionId": 546,
-                       "deploymentId": "bpmDemostracion:ejemploSubida:1.0",
-                       "quickTaskSummary": false,
-                       "parentId": -1,
-                       "potentialOwners": null
-                   },
-                   {
-                       "@class": "org.kie.services.client.serialization.jaxb.impl.task.JaxbTaskSummary",
-                       "id": 802,
-                       "name": "Tarea 1",
-                       "subject": "",
-                       "description": "",
-                       "status": "Completed",
-                       "priority": 0,
-                       "skipable": true,
-                       "actualOwnerId": "bpmadmin",
-                       "createdById": "bpmadmin",
-                       "createdOn": 1463584800605,
-                       "activationTime": 1463584800605,
-                       "expirationTime": null,
-                       "processInstanceId": 26210,
-                       "processId": "ejemploSubida.demoAPI2",
-                       "processSessionId": 546,
-                       "deploymentId": "bpmDemostracion:ejemploSubida:1.0",
-                       "quickTaskSummary": false,
-                       "parentId": -1,
-                       "potentialOwners": null
-                   },
-                   {
-                       "@class": "org.kie.services.client.serialization.jaxb.impl.task.JaxbTaskSummary",
-                       "id": 803,
-                       "name": "Tarea 1",
-                       "subject": "",
-                       "description": "../renovaciones/01-busqueda-clientes.html",
-                       "status": "Reserved",
-                       "priority": 0,
-                       "skipable": true,
-                       "actualOwnerId": "bpmadmin",
-                       "createdById": "bpmadmin",
-                       "createdOn": 1463584801398,
-                       "activationTime": 1463584801398,
-                       "expirationTime": null,
-                       "processInstanceId": 26211,
-                       "processId": "ejemploSubida.demoAPI2",
-                       "processSessionId": 546,
-                       "deploymentId": "bpmDemostracion:ejemploSubida:1.0",
-                       "quickTaskSummary": false,
-                       "parentId": -1,
-                       "potentialOwners": null
-                   },
-                   {
-                       "@class": "org.kie.services.client.serialization.jaxb.impl.task.JaxbTaskSummary",
-                       "id": 804,
-                       "name": "Tarea 2",
-                       "subject": "",
-                       "description": "../renovaciones/02-detalle-cuenta.html",
-                       "status": "Ready",
-                       "priority": 0,
-                       "skipable": true,
-                       "actualOwnerId": "bpmadmin",
-                       "createdById": "bpmadmin",
-                       "createdOn": 1463584825180,
-                       "activationTime": 1463584825180,
-                       "expirationTime": null,
-                       "processInstanceId": 26210,
-                       "processId": "ejemploSubida.demoAPI2",
-                       "processSessionId": 546,
-                       "deploymentId": "bpmDemostracion:ejemploSubida:1.0",
-                       "quickTaskSummary": false,
-                       "parentId": -1,
-                       "potentialOwners": null
-                   }
-                ],
-                "pageNumber": null,
-                "pageSize": null
-            };
-            var countCompletadas = 0;
-            var countEnProceso = 0;
-            var countPendientes = 0;
-            $.each(json.taskSummaryList, function (index, element) {
-                if (element.status == 'Reserved' || element.status == 'Ready') {
-                    countPendientes++;
-                }
-                if (element.status == 'InProgress') {
-                    countEnProceso++;
-                }
-                if (element.status == 'Completed') {
-                    countCompletadas++;
-                }
-            });
+            console.log('Error ejecutando metodo obtieneTareasIndex');
+            var countCompletadas = 25;
+            var countEnProceso = 14;
+            var countPendientes = 9;
             console.log('pendientes, proceso, completadas=' + countPendientes + ',' + countEnProceso + ',' + countCompletadas);
             $('#countCompletadas').text(countCompletadas);
             $('#countEnProceso').text(countEnProceso);
