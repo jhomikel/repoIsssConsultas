a89293be5cbae1d3325d331cd0d094482fe0a890
Jhony Mikel escobar <mikel.escobar@datumredsoft.com>
2016-08-28 17:00:23 -0600

    Paso 03-preparacion, pendiente paso siguiente.

--- isss_consulta/src/main/webapp/js/renovaciones/03-preparacion.js
+++ isss_consulta/src/main/webapp/js/renovaciones/03-preparacion.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     //cargaCredenciales();
     //cargaPaginaIniciaTarea();
     //obtieneNombreCliente(codigoCliente);
-    obtieneDetallesInstanciaProceso($.urlParam('processid'));
+    //obtieneDetallesInstanciaProceso($.urlParam('processid'));
     
     $("#btnSiguiente").click(function(){
         
@@ -11,10 +11,37 @@ $(document).ready(function(){
         var fc = $("#FC").val();
         var fr = $("#FR").val();
         var peso = $("#Peso").val();
-        var talla = $("#Talla").val();
         var tsistolica = $("#TSistolica").val();
         var tdiastolica = $("#TDiastolica").val();
         var codatencion = $.urlParam('processid');
+        
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://192.168.56.102:8080/ISSS_Servicios/webresources/entidades.signos",
+            "method": "POST",
+            "headers": {
+              "content-type": "application/json",
+              "cache-control": "no-cache",
+              "postman-token": "bbfeb3dd-14a3-6ae2-a0c5-ceebcd2d2734"
+            },
+            "processData": false,
+            "data": "{\n\
+                            \"temperatura\":" + temperatura + ",\n\
+                            \"fc\":" + fc + ",\n\
+                            \"fr\":" + fr + ",\n\
+                            \"peso\":" + peso + ",\n\
+                            \"tsistolica\":" + tsistolica + ",\n\
+                            \"tdiastolica\":" + tdiastolica + ",\n\
+                            \"codatencion\":{\n\
+                                \"codatencion\":" + codatencion + "\n\
+                            }\n\
+                     }"
+          }
+
+          $.ajax(settings).done(function (response) {
+            alert('Signos ingresados satisfactoriamente');
+          });
         
         //Operación botón siguiente
     });
