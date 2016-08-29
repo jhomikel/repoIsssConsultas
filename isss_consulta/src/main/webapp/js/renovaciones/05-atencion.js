842cbf444694fd5e1b2d3e6037915ac31a621d4d
Jhony Mikel escobar <mikel.escobar@datumredsoft.com>
2016-08-29 17:19:20 -0600

    Commit 29-08-2016 05:19

--- isss_consulta/src/main/webapp/js/renovaciones/05-atencion.js
+++ isss_consulta/src/main/webapp/js/renovaciones/05-atencion.js
@@ -1,15 +1,42 @@
 $(document).ready(function(){
 
-    cargaCredenciales();
-    cargaPaginaIniciaTarea();
-    obtieneNombreCliente(codigoCliente);
-    obtieneDetallesInstanciaProceso($.urlParam('processid'));
+    //cargaCredenciales();
+    //cargaPaginaIniciaTarea();
+    //obtieneNombreCliente(codigoCliente);
+    //obtieneDetallesInstanciaProceso($.urlParam('processid'));
 
-	var urlExterno = $("#direccion_url").val();
-	var varsFrecuentes = "?processid=" + processid + "&clientesID=" + codigoCliente;
-	var iframeUrl = urlExterno + varsFrecuentes;
+	var numAfiliacion = $.urlParam('clientesID');
+        var codProceso = $.urlParam('processid');
+        
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://192.168.56.102:8080/ISSS_Servicios/webresources/entidades.atencion/" + codProceso,
+            "method": "GET",
+            "headers": {
+              "content-type": "application/json",
+              "cache-control": "no-cache",
+              "postman-token": "d5d9f457-2a1c-dbd1-7d9f-022cfe93b4be"
+            },
+            "processData": false,
+            "data": ""
+          }
 
-	$("#myIframe").attr('src',iframeUrl);
+          $.ajax(settings).done(function (datos) {
+            console.log("Proceso: " + codProceso)
+            html = '<h3>Medico: ' + datos.codcita.codmedico.codmedico + ' | ' + datos.codcita.codmedico.nombres + ' ' + datos.codcita.codmedico.apellidos + '</h3>';
+            html += '<div class="panel panel-default">';
+            html += '<div class="panel-body">';
+            html += 'Codigo de afiliaci&oacute;n: <b>' + datos.codcita.numafiliacion.numafiliacion + '</b><br>';
+            html += 'Tipo de afiliado: <b>' + datos.codcita.tipo + '</b><br>';
+            html += 'Ocupaci&oacute;n del afiliado: <b>' + datos.codcita.numafiliacion.ocupacion + '</b><br>';            
+            html += 'Tipo de consulta: <b>' + datos.codcita.codmedico.codespecialidad.nombespecialidad + '</b><br>';
+            html += '</div>';
+            html += '</div>';
+            document.getElementById('atencion').innerHTML = html;          
+          });
+          
+          
 
 	$("#btnSiguiente").click(botonSiguientePaso4);
 	$("#btnTerminar").click($.botonTerminar);
